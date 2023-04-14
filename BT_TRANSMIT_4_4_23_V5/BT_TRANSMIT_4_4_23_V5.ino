c5f1ae88904c791f98de59c6c20fecf8caefbf02
wire-fire <jackmctasney03@gmail.com>
2023-04-14 10:36:31 -0400

    Updated mapping only in joystick

---
+++ BT_TRANSMIT_4_4_23_V5/BT_TRANSMIT_4_4_23_V5.ino
@@ -0,0 +1,77 @@
+#include <AltSoftSerial.h>
+AltSoftSerial BTserial;
+int x = A0;
+int y = A1;
+int joystickX = 0; //Holds joystick x value for transmission (STEERING)
+int joystickY = 0; //Holds joystick y value for transmission (THROTTLE)
+int storedX = 0; //Holds analog value for curve
+int storedY = 0; //Holds analog value for curve
+int scaleFactor = 10; //Variable for scaling output
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(x, INPUT);
+  pinMode(y, INPUT);
+  BTserial.begin(9600);
+  Serial.begin(9600);
+  BTserial.print("AT");
+  delay(1000);
+  BTserial.print("AT+CO094A9A83AED70");
+  delay(1000);
+  BTserial.print("AT+CO094A9A83AED70");
+  delay(1000);
+  BTserial.print("AT+CO094A9A83AED70");
+  storedX = map(analogRead(x), 0, 1023, 1200, 1800);
+  storedY = map(analogRead(y), 0, 1023, 1000, 2000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  joystickX = map(analogRead(x), 0, 1023, 1200, 1800);
+  joystickY = map(analogRead(y), 0, 1023, 1000, 2000);
+
+  //Acceleration Curve
+  if (joystickX > storedX) { //If joystick value is larger than stored value (X)
+    storedX += scaleFactor; 
+  }
+  else if (joystickX < storedX) {
+    storedX -= scaleFactor;
+  }
+  else {
+    storedX = storedX;
+  }
+  if (joystickY > storedY) { //If joystick value is larger than stored value (X)
+    storedY += scaleFactor;
+  }
+  else if (joystickY < storedY) {
+    storedY -= scaleFactor;
+  }
+  else {
+    storedY = storedY;
+  }
+
+  //Centering forgiveness
+  if (400 < joystickX && joystickX < 600)
+  {
+    storedX = 1470; //Experimentally determined center value (STEERING)
+  }
+  if (400 < joystickY && joystickY < 600)
+  {
+    storedY = 1557; //Experimentally determined center value (THROTTLE)
+  }
+
+
+  Serial.print("{");
+  Serial.print(storedX);
+  Serial.print(",");
+  Serial.print(storedY);
+  Serial.print("}\n");
+
+  BTserial.print("{");
+  BTserial.print(storedX);
+  BTserial.print(",");
+  BTserial.print(storedY);
+  BTserial.print("}\n");
+  delay(20);
+}
---
+++ SERVO_BULLSHIT_4_4_23_V6/SERVO_BULLSHIT_4_4_23_V6.ino
@@ -0,0 +1,57 @@
+#include <Servo.h>
+const byte ledPin = 13;
+const byte interruptPin = 2;
+volatile byte state = LOW;
+String test = "";
+char c;
+Servo ch1;
+Servo ch2;
+int ch1Pin = 5;
+int ch2Pin = 6;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), blink_, CHANGE);
+  ch1.attach(ch1Pin);
+  ch2.attach(ch2Pin);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+
+void blink_() {
+  state = !state;
+  //Serial.println("I AM HERE");
+  if (c == '}') {
+    //Serial.print(test);
+    int ind1 = test.indexOf("{"); //Finds { index
+    int ind2 = test.indexOf(","); //Finds , index
+    int ind3 = test.indexOf("}"); //Finds } index
+    String strX = test.substring(ind1 + 1, ind2); //captures first data String
+    String strY = test.substring(ind2 + 1, ind3); //captures second data String
+    int ch1Width = strX.toInt();
+    int ch2Width = strY.toInt();
+    Serial.print(ch1Width);
+    Serial.print(", ");
+    Serial.println(ch2Width);
+    ch1.writeMicroseconds(ch1Width);
+    ch2.writeMicroseconds(ch2Width);
+    test="";
+    c = ' ';
+  }
+  else
+  {
+    while (Serial.available())
+    {
+      c = Serial.read();
+      test += c;
+    }
+  }
+    digitalWrite(ledPin, state);
+  }
